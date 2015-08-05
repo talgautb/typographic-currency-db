cebce1d875e2e38974908a8487bbeb0ce8956171
talgautb <talgautb@yandex.kz>
2015-08-05 14:51:44 +0600

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# typographic-currency-db
+
+[![NPM version][npm-image]][npm-url]
+
+> Raw data about typographic currency
+
+## Install
+
+    npm install --save typographic-currency-db
+
+## License
+
+MIT © [talgautb](http://gtalk.kz)
+
+[npm-url]: https://npmjs.org/package/typographic-currency-db
+[npm-image]: https://img.shields.io/npm/v/typographic-currency-db.svg?style=flat-square
+
---
+++ index.js
@@ -0,0 +1,51 @@
+// ISO_4217: unicode // symbol
+// http://www.unicode.org/charts/PDF/U20A0.pdf
+
+module.exports = {
+  'AFN': '\u060b', // ؋
+  'AMD': '\u058f', // ֏
+  'ANG': '\u0192', // ƒ
+  'AWG': '\u0192', // ƒ
+  'AZN': '\u20bc', // ₼
+  'BDT': '\u09f3', // ৳
+  'BTC': '\u0243', // Ƀ
+  'CNY': '\u00a5', // ¥
+  'CRC': '\u20a1', // ₡
+  'EUR': '\u20ac', // €
+  'FKP': '\u00a3', // £
+  'GBP': '\u00a3', // £
+  'GHS': '\u20b5', // ₵
+  'GIP': '\u00a3', // £
+  'ILS': '\u20aa', // ₪
+  'INR': '\u20b9', // ₹
+  'IRR': '\ufdfc', // ﷼
+  'JPY': '\u00a5', // ¥
+  'KHR': '\u17db', // ៛
+  'KPW': '\u20a9', // ₩
+  'KRW': '\u20a9', // ₩
+  'KZT': '\u20b8', // ₸
+  'LAK': '\u20ad', // ₭
+  'LKR': '\u20a8', // ₨
+  'MNT': '\u20ae', // ₮
+  'MTL': '\u20a4', // ₤
+  'MUR': '\u20a8', // ₨
+  'NGN': '\u20a6', // ₦
+  'NPR': '\u20a8', // ₨
+  'OMR': '\ufdfc', // ﷼
+  'PHP': '\u20b1', // ₱
+  'PKR': '\u20a8', // ₨
+  'PYG': '\u20b2', // ₲
+  'QAR': '\ufdfc', // ﷼
+  'RUB': '\u20BD', // ₽
+  'SAR': '\ufdfc', // ﷼
+  'SCR': '\u20a8', // ₨
+  'SDG': '\u00a3', // £
+  'SHP': '\u00a3', // £
+  'SVC': '\u20a1', // ₡
+  'SYP': '\u00a3', // £
+  'THB': '\u0e3f', // ฿
+  'UAH': '\u20b4', // ₴
+  'USD': '\u0024', // $
+  'VND': '\u20ab', // ₫
+  'YER': '\ufdfc'  // ﷼
+}
